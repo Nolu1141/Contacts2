0d0d94ca77697e303fb065b5ecd2fac6a7b0be02
Nolu1141 <Nolu@mail.ru>
2018-05-19 16:24:02 +0300

    Initial commit

---
+++ src/main/java/com/contactspring/ContactController.java
@@ -0,0 +1,34 @@
+package com.contactspring;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@RequestMapping("/contacts")
+@Controller
+public class ContactController {
+
+    private final Logger logger = LoggerFactory.getLogger(ContactController.class);
+
+    private ContactService contactService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model model) {
+        logger.info("Listing contacts");
+        List<Contact> contacts = contactService.findAll();
+        model.addAttribute("contacts", contacts);
+        logger.info("No. of contacts: "+ contacts.size());
+        return "contacts/list";
+    }
+    @Autowired
+    public void setContactService(ContactService contactService) {
+        this.contactService = contactService;
+    }
+}
---
+++ src/main/java/com/contactspring/ContactService.java
@@ -0,0 +1,11 @@
+package com.contactspring;
+
+
+
+import java.util.List;
+
+public interface ContactService {
+    List<Contact> findAll();
+    Contact findById(Long id);
+    Contact save (Contact contact);
+}
